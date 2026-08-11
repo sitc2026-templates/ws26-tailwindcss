edafad17215cd1517df4bced9eb30e963d3c4513
Hrustinszki Ádám <hrustinszkiadam@gmail.com>
2026-08-11 14:28:07 +0200

    Add allowed host '.skillsit.eu' to Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  server: { host: true, port: 80, strictPort: true, allowedHosts: ['.skill17.com'] },
+  server: { host: true, port: 80, strictPort: true, allowedHosts: ['.skill17.com', '.skillsit.eu'] },
 })
